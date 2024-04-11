5cd5466bf8b21db98b8926fcacc83d1149d02c5b
SebasMarv <sebas.mar062699@gmail.com>
2024-04-11 14:23:19 -0500

    Modify 11.04.2024

--- 02-Java/src/E_matricesVectores/Matrices.java
+++ 02-Java/src/E_matricesVectores/Matrices.java
@@ -1,5 +1,7 @@
 package E_matricesVectores;
 
+import java.util.Scanner;
+
 public class Matrices {
     public static void main(String[] args) {
 
@@ -7,11 +9,35 @@ public class Matrices {
         int matriz [][] = new int[3][3];
 
         // Asignacion manual
-        matriz[0][0] = 1;
-        matriz[0][1] = 2;
-        matriz[0][2] = 3;
-        matriz[1][0] = 4;
-        matriz[1][1] = 5;
-        matriz[1][2] = 6;
+        // matriz[0][0] = 1;
+        // matriz[0][1] = 2;
+        // matriz[0][2] = 3;
+        // matriz[1][0] = 4;
+        // matriz[1][1] = 5;
+        // matriz[1][2] = 6;
+        // matriz[2][0] = 7;
+        // matriz[2][1] = 8;
+        // matriz[2][2] = 9;
+
+        Scanner input = new Scanner(System.in);
+        
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("El valor de la posicion ["+ i +"]["+j+"]: ");
+                matriz[i][j] = input.nextInt();
+            }
+        }
+
+        input.close();
+
+        System.out.println("------------------------------------------");
+
+        // Recorrido
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("El valor de la posicion ["+ i +"]["+j+"]: "+ matriz[i][j]);
+            }
+        }
     }
 }
