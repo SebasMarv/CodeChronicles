740f6401fe5fd0bf8842edeee76baf0358f2c751
Sebastian M. <sebas.mar062699@gmail.com>
2024-04-12 19:42:25 -0500

    modify 12.04.2024

---
+++ 02-Java/src/E_matricesVectores/EjercicioIntegradorMatrizIfFor.java
@@ -0,0 +1,7 @@
+package E_matricesVectores;
+
+public class EjercicioIntegradorMatrizIfFor {
+    public static void main(String[] args) {
+        
+    }
+}
--- 02-Java/src/E_matricesVectores/EjercicioIntegradorVectIfFor.java
+++ 02-Java/src/E_matricesVectores/EjercicioIntegradorVectIfFor.java
@@ -8,29 +8,46 @@ veces se cargó el número 3*/
 
 public class EjercicioIntegradorVectIfFor {
     public static void main(String[] args) {
+
         int vector [] = new int[15];
 
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Ingrese la opción [1-Seguir] o [2-No seguir]");
+        // int desi = input.nextInt();
+        // if (desi == 1) {
+        //     for (int i=0; i<15; i++) {
+        //         System.out.println("Ingrese su numero en la posición ["+ i + "]");
+        //         vector[i] = input.nextInt();
+        //     }
+        //     System.out.println("-----------------------------------------------");
+        //     for (int i=0; i<=vector.length; i++) {
+        //         if (vector[i] == 3) {
+        //             System.out.println("El numero de posición ["+ i + "] es: "+ vector[i]);
+        //         } else {
+        //             ;
+        //         }
+        //     }
+        // } else if (desi == 2) {
+        //     System.out.println("Hasta luego");
+        // } else {
+        //     System.out.println("Ingrese un numero valido.");
+        // }
+        // input.close();
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Ingrese la opción [1-Seguir] o [2-No seguir]");
-        int desi = input.nextInt();
-        if (desi == 1) {
-            for (int i=0; i<15; i++) {
-                System.out.println("Ingrese su numero en la posición ["+ i + "]");
-                vector[i] = input.nextInt();
+
+        for (int i = 0; i<15; i++) {
+            vector[i] = input.nextInt();
+        }
+
+        // recorrer y contar cuentos numeros hay
+        int cont = 0;
+        for (int i = 0; i<15; i++) {
+            if (vector[i] == 3) {
+                cont = cont + 1;
             }
-            System.out.println("-----------------------------------------------");
-            for (int i=0; i<=vector.length; i++) {
-                if (vector[i] == 3) {
-                    System.out.println("El numero de posición ["+ i + "] es: "+ vector[i]);
-                } else {
-                    ;
-                }
-            }
-        } else if (desi == 2) {
-            System.out.println("Hasta luego");
-        } else {
-            System.out.println("Ingrese un numero valido.");
         }
         input.close();
+        System.out.println("Cantidad de numeros 3: "+cont);
     }
 }
