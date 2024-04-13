6db71aa898166c7037daff9998502e7e97bb2fb2
Sebastian M. <sebas.mar062699@gmail.com>
2024-04-13 08:40:02 -0500

    modify 13.04.2024

---
+++ 02-Java/Java/src/F_ModeloCapasLogicaYPersistencia/logica/PruebaCapas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package F_ModeloCapasLogicaYPersistencia.logica;
+
+import F_ModeloCapasLogicaYPersistencia.igu.Pantalla;
+
+/**
+ *
+ * @author USER
+ */
+public class PruebaCapas {
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        // Hacer aparecer al medio de la pantalla
+        panta.setLocationRelativeTo(null);
+    }
+}
