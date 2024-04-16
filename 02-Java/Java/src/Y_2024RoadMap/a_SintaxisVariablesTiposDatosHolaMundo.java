282e8107297713ca1e57f06d6043a9b17c16ed50
Sebastian M. <sebas.mar062699@gmail.com>
2024-04-16 03:04:58 -0500

    Modify 15.04.2024

---
+++ 02-Java/Java/src/Y_2024RoadMap/a_SintaxisVariablesTiposDatosHolaMundo.java
@@ -0,0 +1,43 @@
+package Y_2024RoadMap;
+
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado. ☑️
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...). ☑️
+ * - Crea una variable (y una constante si el lenguaje lo soporta). ☑️
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+public class a_SintaxisVariablesTiposDatosHolaMundo {
+    public static void main(String[] args) {
+        //https://www.java.com/es/
+        
+        /*
+        https://www.java.com/es/
+        */
+        
+        // Variable
+        int variable = 2;
+        
+        // Constante
+        final int CONSTANTE = 3;
+        
+        // Declaración de variables primitivas del lenguaje JAVA
+    }
+}
