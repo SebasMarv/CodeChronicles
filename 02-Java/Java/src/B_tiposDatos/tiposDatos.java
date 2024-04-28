545c3d666569d65244b10db9c5d408721a001204
Sebastian M. <sebas.mar062699@gmail.com>
2024-04-27 21:26:32 -0500

    Modify 27.04.2024

--- 02-Java/Java/src/B_tiposDatos/tiposDatos.java
+++ 02-Java/Java/src/B_tiposDatos/tiposDatos.java
@@ -10,13 +10,72 @@ package B_tiposDatos;
  */
 public class tiposDatos {
     public static void main(String[] args) {
-        int numero = 5;
-        String nombre = "Sebas";
-        char letra = 'S';
-        double temperatura = 38.5;
-        boolean siONo = false;
-        long largo = 123456789;
 
-        System.out.println(numero + "-" + nombre + "-" + letra + "-" + temperatura + "-" + siONo + "-" + largo);
+        // Constantes
+        final double igv = 0.18;
+
+        // Variables
+        // Enteros
+        byte diasMes = 31; // Aproximadamente
+        short diasLustro = (12 * 31) * 5;
+        int velocidadLuz = 299792458;
+        long añoLuz = velocidadLuz * 365;
+
+        // Decimales
+        float pi = 3.1415926535f;
+        double e = 2.718281828459045235360;
+
+        System.out.println("-----------------Tipos de Datos en Java-----------------");
+        System.out.println();
+
+        // Caracteres
+        char letraA = 'A';
+
+        // Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // Imprimir Enteros
+        System.out.println("byte: " + diasMes);
+        System.out.println("short: " + diasLustro);
+        System.out.println("int: " + velocidadLuz);
+        System.out.println("long: " + añoLuz);
+
+        // Imprimir Decimales
+        System.out.println("float: " + pi);
+        System.out.println("double: " + e);
+
+        // Imprimir Caracteres
+        System.out.println("char: " + letraA);
+
+        // Imprimir Booleanos
+        System.out.println("boolean: " + verdadero);
+        System.out.println("boolean: " + falso);
+
+        // Imprimir Constantes
+        System.out.println("Constante: " + igv);
+
+        System.out.println();
+        // Palabras reservadas de Java
+        System.out.println("-----------------Palabras reservadas de Java-----------------");
+        System.out.println();
+        String palabrasReservadas = "abstract, assert, boolean, break, byte, case, catch, char, class, const, continue, default, do, double, else, enum, extends, final, finally, float, for, goto, if, implements, import, instanceof, int, interface, long, native, new, package, private, protected, public, return, short, static, strictfp, super, switch, synchronized, this, throw, throws, transient, try, void, volatile, while";
+        String[] palabrasArray = palabrasReservadas.split(", ");
+
+        int columnas = 5;
+        int filas = (int) Math.ceil((double) palabrasArray.length / columnas);
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                int index = i + j * filas;
+                if (index < palabrasArray.length) {
+                    System.out.printf("%-12s", palabrasArray[index]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Añadir que tambien se incluyen las palabras true, false y null.");
+        System.out.println();
     }
 }
--- 02-Java/Java/src/C_operadores/Operadores.java
+++ 02-Java/Java/src/C_operadores/Operadores.java
@@ -10,14 +10,97 @@ package C_operadores;
  */
 public class Operadores {
     public static void main(String[] args) {
-        
-        int num1, num2, resultado;
 
-        num1 = 4;
-        num2 = 2;
+        // int num1, num2, resultado;
+        // num1 = 4;
+        // num2 = 2;
+        // resultado = num1 * num2;
+        // System.out.println("El resultado de la multiplicacion es de: "+resultado);
 
-        resultado = num1 * num2;
-        System.out.println("El resultado de la multiplicacion es de: "+resultado);
+        int variableX = 50, variableY = 10;
+        int resultado;
 
-    }    
+        // Operadores Aritmeticos - Básicos
+
+        resultado = variableX + variableY;
+        System.out.println(resultado);
+
+        resultado = variableX - variableY;
+        System.out.println(resultado);
+
+        resultado = variableX * variableY;
+        System.out.println(resultado);
+
+        resultado = variableX / variableY;
+        System.out.println(resultado);
+
+        // Operadores Aritmeticos - Intermedios
+        // -- -> Decremento
+        // ++ -> Incremento
+
+        // variableX = variableX + 1;
+        // variableX++;
+        variableX += 1;
+        System.out.println(variableX);
+
+        // variableX = variableX - 1;
+        // variableX--;
+        variableX -= 1;
+        System.out.println(variableX);
+
+        variableX *= 2;
+        System.out.println(variableX);
+
+        variableX /= 2;
+        System.out.println(variableX);
+
+        System.out.println(
+                "Status de variables: \nvariableX: " + variableX + "\nvariableY: " + variableY + "\nresultado: "
+                        + resultado);
+
+        // Cast
+
+        float variableZ = 5.5f;
+
+        // Consiste de convertir de forma espontanea la conversion del tipo de datos
+        // de una variable a otra, en este caso de float a int
+        resultado = variableX + (int) variableZ;
+        System.out.println(resultado + " " + (int) variableZ);
+
+        // Operadores relacionales
+        int variablel = 300, variable2 = 200;
+        boolean resultado2;
+
+        resultado2 = variablel > variable2;
+        System.out.println(resultado2);
+        resultado2 = variablel < variable2;
+        System.out.println(resultado2);
+        resultado2 = variablel >= variable2;
+        System.out.println(resultado2);
+        resultado2 = variablel <= variable2;
+        System.out.println(resultado2);
+        resultado2 = variablel == variable2;
+        System.out.println(resultado2);
+        resultado2 = variablel != variable2;
+        System.out.println(resultado2);
+
+        // Operador.es lógicos
+        // and (y), or (o) y not (no)
+        boolean resultad3;
+        resultad3 = 5 >= 5 && true && 10 > 9;
+        System.out.println(resultad3);
+        resultad3 = false || false || false || true;
+        System.out.println(resultad3);
+        resultad3 = (5 >= (2 * 3) || true) && (true && 10 > 5);
+        System.out.println(resultad3);
+        System.out.println(!false);
+
+        // Operador ternario
+        // IFTTT
+        // expresionl ? expresion2 : expresion3
+        String mensaje = 10 > 90 ? "Es mayor" : "Es menor";
+        System.out.println(mensaje);
+
+        String mensajee = mensaje.concat(" que el otro", "hoal");
+    }
 }
